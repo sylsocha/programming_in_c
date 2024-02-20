35a5b0f32208a9474dce0bf83c2e30cc8260b22e
sylwucha <97919396+sylsocha@users.noreply.github.com>
2024-02-20 12:59:05 +0100

    Update zadanie5.c

--- lab1_20.02.2024r./zadanie5.c
+++ lab1_20.02.2024r./zadanie5.c
@@ -1,1 +1,20 @@
 
+#include <stdio.h>
+
+int main()
+{
+    int max, wynik;
+    printf("Podaj ograniczenie: ");
+    scanf("%d", &max);
+    
+    for(int i == 1; i<max; i ++)
+    {
+        if(i%3==0 && i%5==0)
+        else if(i%5==0) wynik+=i;
+        else if(i%3) wynik +=i;
+    }
+    
+    return 0;
+}
+
+//dokończyć
