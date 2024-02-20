20363cc896a465aaaa9e46a628e12e023f3b101a
sylwucha <97919396+sylsocha@users.noreply.github.com>
2024-02-20 11:58:07 +0100

    Create zadanie1.c

---
+++ zadanie1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    char imie[20];
+    printf("Podaj swoje imię: ");
+    scanf("%s", imie);
+    
+    printf("Siemano, %s", imie);
+
+    return 0;
+}
