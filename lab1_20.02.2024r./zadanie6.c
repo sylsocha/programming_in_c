2f16a8a67be33822d46fcd1a972c7f0c33851e19
sylwucha <97919396+sylsocha@users.noreply.github.com>
2024-02-26 09:07:48 +0100

    Update zadanie6.c

--- lab1_20.02.2024r./zadanie6.c
+++ lab1_20.02.2024r./zadanie6.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    char figury[3][10] = {"kamień", "papier", "nożyce"};
+    int n;
+    printf("Ile rund chcesz rozegrać?")
+    scanf("%d", &n)
+    
+    float* wyniki = malloc((n+1) * sizeof(*a));
+    for(int i=0;i<3;i++){
+        printf("%s\n", figury[i]);
+    }
+    
+    return 0;
+}
