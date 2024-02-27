eac4d2a6986400e6063faa234737e59e86a3a136
sylwucha <97919396+sylsocha@users.noreply.github.com>
2024-02-27 09:30:34 +0100

    Update zadanie6.c

--- lab1_20.02.2024r./zadanie6.c
+++ lab1_20.02.2024r./zadanie6.c
@@ -6,9 +6,8 @@ int main()
 {
     char figury[3][7] = {"kamień", "papier", "nożyce"};
     int rundy;
-    int wyniki = 3;
     char komputer;
-    char gracz[7];
+    int gracz;
     
     printf("Ile rund chcesz rozegrać? ");
     scanf("%d", &rundy);
@@ -22,13 +21,20 @@ int main()
         printf("%d\n", los%3);
         komputer = los%3;
         
-        printf("Podaj swoją figurę: ");
-        scanf("%s", gracz);
+        printf("Podaj numer swojej figury\n1 - kamień\n2 - papier\n3 - nożyce: ");
+        scanf("%d", &gracz);
+        gracz -=1;
         
         switch(gracz)
         {
-            case 
+            case 0:
+                switch(komputer)
+                {
+                    case 0:
+                    break;
+                }
         }
+        
     }
     
     return 0;
