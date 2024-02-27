136108ec8e736e24cc627575c8bb93c5c7980560
sylwucha <97919396+sylsocha@users.noreply.github.com>
2024-02-27 09:02:23 +0100

    Update zadanie6.c

--- lab1_20.02.2024r./zadanie6.c
+++ lab1_20.02.2024r./zadanie6.c
@@ -4,9 +4,11 @@
 
 int main()
 {
-    char figury[3][10] = {"kamień", "papier", "nożyce"};
+    char figury[3][7] = {"kamień", "papier", "nożyce"};
     int rundy;
     int wyniki = 3;
+    char komputer;
+    char gracz[7];
     
     printf("Ile rund chcesz rozegrać? ");
     scanf("%d", &rundy);
@@ -16,8 +18,17 @@ int main()
     srand(time(NULL));
     
     for(int i=0;i<rundy;i++){
-        char los = rand();
-        printf("%d\n", los);
+        unsigned char los = rand();
+        printf("%d\n", los%3);
+        komputer = los%3;
+        
+        printf("Podaj swoją figurę: ");
+        scanf("%s", gracz);
+        
+        switch(gracz)
+        {
+            case 
+        }
     }
     
     return 0;
