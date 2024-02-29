3681361f3a43885cb2092c87fd6ba1940f9edd19
sylwucha <97919396+sylsocha@users.noreply.github.com>
2024-02-29 23:28:10 +0100

    Update zadanie2.c

--- lab2_27.02.2024r./zadanie2.c
+++ lab2_27.02.2024r./zadanie2.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+float doubled(float *x)
+{
+    return 2**x;
+}
+
+int main()
+{
+    float x = 3.1415;
+    printf("2x = %f", doubled(&x));
+    
+  return 0;
+}
