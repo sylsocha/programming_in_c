3afea2d019ab286246fe531b95a9333d73f199b5
sylwucha <97919396+sylsocha@users.noreply.github.com>
2024-03-01 08:07:38 +0100

    Update zadanie3.c

--- lab2_27.02.2024r./zadanie3.c
+++ lab2_27.02.2024r./zadanie3.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef struct
+{
+    int x;
+    int y;
+} Point;
+
+void zmienWspolrzedne(int *x, int *y)
+{
+    (*x) *=5;
+    (*y) *=3;
+}
+
+typedef struct
+{
+    Point a;
+    Point b;
+    Point c;
+    Point d;
+} Rectangle;
+
+void poleIObwod(Point *a, Point *b, Point *c, Point *d)
+{
+    
+}
+
+int main()
+{
+  Point p;
+  p.x = 2;
+  p.y = 6;
+  zmienWspolrzedne(&p.x, &p.y);
+  printf("%d, %d", p.x, p.y);
+}
