831b58300af0c3f113e02f18b13a4f320cf1d662
sylwucha <97919396+sylsocha@users.noreply.github.com>
2024-03-01 13:36:01 +0100

    Update zadanie4.c

--- lab2_27.02.2024r./zadanie4.c
+++ lab2_27.02.2024r./zadanie4.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef struct
+{
+    char * imie;
+    float ocena;
+    
+} Student;
+
+float quicksort()
+{
+    return 0;
+}
+
+int main()
+{
+    Student * tabStudentow;
+    tabStudentow[0].imie[0] = "Anna";
+    tabStudentow[0].ocena = 3.5;
+    printf("%s", tabStudentow[0].imie);
+    
+    return 0;
+}
+
+// SKOŃĆZYĆ
