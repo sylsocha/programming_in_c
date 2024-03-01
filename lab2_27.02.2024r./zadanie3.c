2fc93639094314e4ef23b810f9cc6ad3cdd45332
sylwucha <97919396+sylsocha@users.noreply.github.com>
2024-03-01 08:16:15 +0100

    Update zadanie3.c

--- lab2_27.02.2024r./zadanie3.c
+++ lab2_27.02.2024r./zadanie3.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 typedef struct
 {
@@ -21,6 +22,11 @@ typedef struct
     Point d;
 } Rectangle;
 
+float dlugoscOdcinka(Point *a, Point *b)
+{
+    return pow(((*a).x - (*b).x), 2);
+}
+
 void poleIObwod(Point *a, Point *b, Point *c, Point *d)
 {
     
@@ -31,6 +37,12 @@ int main()
   Point p;
   p.x = 2;
   p.y = 6;
+  
+  Point r;
+  r.x = 1;
+  r.y = 4;
   zmienWspolrzedne(&p.x, &p.y);
-  printf("%d, %d", p.x, p.y);
+  printf("%d, %d\n", p.x, p.y);
+  
+  printf("%d", dlugoscOdcinka(&p, &r));
 }
