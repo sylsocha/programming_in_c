2392fad6c2f340137eb9591a33b6223fcf9aeded
sylwucha <97919396+sylsocha@users.noreply.github.com>
2024-03-19 21:58:56 +0100

    Update zadanie1.c

--- lab4_12.03.2024r./zadanie1.c
+++ lab4_12.03.2024r./zadanie1.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int factorial(int x){
+
+    if(x == 1){
+        return x;
+    }
+    else if(x>1){
+        return x * factorial(x-1);
+    }
+    return 0;
+}
+
+int main(){
+
+    int x = 5;
+    printf("silnia 5 == %d", factorial(x));
+
+    return 0;
+}
