6e4f3d89107be8c42ca0279b141e265284488722
sylwucha <97919396+sylsocha@users.noreply.github.com>
2024-03-22 14:33:33 +0100

    Create zadanie2.c

---
+++ lab3_5_03_2024r/zadanie2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <malloc.h>
+
+int main()
+{
+    int wiersze, kolumny;
+    printf("Podaj liczbe wierszy");
+    scanf("%d", &wiersze);
+    printf("Podaj liczbe kolumn");
+    scanf("%d", &kolumny);
+
+    int **macierz = (int **) malloc(wiersze * sizeof(int *));
+
+    for(int i = 0; i <wiersze; i++)
+    {
+        macierz[i] = (int *) malloc(kolumny * sizeof(int));
+        for(int j = 0; j<kolumny; j++)
+        {
+           macierz[i][j] = rand();
+        }
+    }
+
+    for(int i = 0; i <wiersze; i++)
+    {
+        for(int j = 0; j<kolumny; j++)
+        {
+            printf("%d ", macierz[i][j]);
+        }
+        printf("\n");
+    }
+
+    free(macierz);
+    return 0;
+}
