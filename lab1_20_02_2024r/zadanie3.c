b0f35282a435bfae7473d3b0f84e92a4fa0396a5
sylwucha <97919396+sylsocha@users.noreply.github.com>
2024-03-22 14:23:53 +0100

    Create zadanie3.c

---
+++ lab1_20_02_2024r/zadanie3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    
+    silnia(array);
+    return 0;
+}
+
+void silnia(int array[10]){
+    int silnie[10];
+    silnie[0] = array[0];
+    printf("silnia %d to %d\n", array[0], silnie[0]);
+    
+    for(int i = 1; i<10; i++)
+    {
+        silnie[i] = silnie[i-1]*array[i];
+        printf("silnia %d to %d\n", array[i], silnie[i]);
+    }
+}
