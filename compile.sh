7e4568e46087fadae8e509202b295621848403e3
sylwucha <97919396+sylsocha@users.noreply.github.com>
2024-03-29 23:05:54 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -5,19 +5,8 @@ find . -name '*.c' | while read file; do
     if [ -f "$file" ]; then
         filename=$(basename "$file" .c)
         output_path=$(dirname "$file")
-        # Check if math.h exists
-        if grep -q '#include *<math\.h>' "$file"; then
-            # Check the operating system
-            if [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-                clang -o "${output_path}/${filename}.exe" "$file" -lm -L/path/to/directory/containing/math/library
-            else
-                clang -o "${output_path}/${filename}" "$file" -lm
-                clang -o "${output_path}/${filename}.exe" "$file" -lm
-            fi
-        else
-            clang -o "${output_path}/${filename}" "$file"
-            clang -o "${output_path}/${filename}.exe" "$file"
-        fi
+        clang -o "${output_path}/${filename}" "$file"
+        clang -o "${output_path}/${filename}.exe" "$file"
         if [ $? -eq 0 ]; then
             echo "Compilation successful for $file"
         else
